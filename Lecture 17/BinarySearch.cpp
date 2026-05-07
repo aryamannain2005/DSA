cc8151df71ef60917c9dcd14550108a558cdbcc6
Aryaman Nain <aryaman.23@lpu.in>
2026-05-07 18:10:42 +0530

    done

---
+++ Lecture 17/BinarySearch.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int BinarySearch(vector<int> arr, int tar){
+    int st = 0;
+    int end = arr.size()-1;
+
+    while(st<=end){
+        int mid = (st+end)/2;
+        if(tar > arr[mid]){
+            st = mid+1;
+        }else if(tar<arr[mid]){
+            end = mid-1;
+        }else{
+            return mid;
+        }
+    }
+    return -1;
+}
+int main(){
+    // vector<int> arr = {-1,0,3,4,5,9,12};
+    // int tar = 12;
+    // cout<<"the target is at: "<<BinarySearch(arr, tar)<<endl;
+    // return 0;
+
+    vector<int> arr1 = {-1,0,3,4,5,9,12};
+    int tar1 = -1;
+    cout<<"the target is at: "<<BinarySearch(arr1, tar1)<<endl;
+
+}
