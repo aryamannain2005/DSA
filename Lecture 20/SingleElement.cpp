2067f430ca2384e0934c29e2fb6dd97abe1a2cf1
Aryaman Nain <aryaman.23@lpu.in>
2026-05-17 16:42:37 +0530

    done

--- Lecture 20/SingleElement.cpp
+++ Lecture 20/SingleElement.cpp
@@ -3,22 +3,26 @@
 using namespace std;
 
 int SingleElement(vector<int>& A){
+    int n = A.size();
     int st = 1;
-    int end = A.size()-1;
+    int end = n-2;
 
     while(st<=end){
         int mid = st+(end-st)/2;
+        if(mid == 0 && A[0] != A[1]) return A[mid];
+        if(mid == n-1 && A[n-1] != A[n-2]) return A[mid];
+
         if(A[mid] != A[mid-1] && A[mid] != A[mid+1]){
-            return mid;
+            return A[mid];
         }
         if(mid%2 == 0){
-            if(A[mid]== mid-1){
+            if(A[mid-1] == A[mid]){
                 end = mid-1;
             }else{
                 st = mid+1;
             }
         }else{
-            if(A[mid] == mid+1){
+            if(A[mid-1] == A[mid]){
                 st = mid+1;
             }else{
                 end = mid-1;
